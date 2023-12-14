a5780a52d6dbe4b1b14f31329898089ff7b4b8e8
window-ops <vladbruma2020@gmail.com>
2023-12-14 13:11:58 +0000

    Update 4 files
    - /css/default.css
    - /images/background.jpg
    - /js/functions.js
    - /index.html

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+var date = new Date();
+var currentYear = date.getFullYear();
+var theFirstIndustrialRevolution = 1760;
+var crashRateinYears = 10;
+var baseOvershootYear = 1971;
+var numberOfYearsToDoubleGlobalConsumption = 62;
+var predictedYearOfCollapse = 2040;
+
+function knowHowOutdatedIsIGC() {
+  var outdatedYears = currentYear - theFirstIndustrialRevolution;
+  return outdatedYears;
+}
+function knowHowManyCrashesHaveOccured() {
+  var crashes = Math.floor((currentYear - theFirstIndustrialRevolution) / crashRateinYears);
+  return crashes;
+}
+function calculateEarthOvershoot() {
+  var earthsUsed = 1 + ((currentYear - baseOvershootYear) / numberOfYearsToDoubleGlobalConsumption);
+  return earthsUsed.toFixed(1);
+}
+function knowWhenSocietyWillCollapse() {
+  var yearsCollapse = predictedYearOfCollapse - currentYear;
+  return yearsCollapse;
+}
+function display() {
+  document.getElementById("howOldInYears").textContent = knowHowOutdatedIsIGC();
+  document.getElementById("howManyCrashes").textContent = knowHowManyCrashesHaveOccured();
+  document.getElementById("earthOvershoot").textContent = calculateEarthOvershoot();
+  document.getElementById("societyCollapse").textContent = knowWhenSocietyWillCollapse();
+}
+document.addEventListener("DOMContentLoaded", function() {
+  display()
+})
